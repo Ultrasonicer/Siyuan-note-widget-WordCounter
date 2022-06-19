f78af7d72bd96af56574c9ebddfa6a6028a5a318
weihan-Chen <weihan-Chen@qq.com>
2022-06-19 23:56:57 +0800

    update WordCounter to v0.0.3.20220619

--- script/count.js
+++ script/count.js
@@ -3,13 +3,13 @@ import { getBlockByID } from './api.js';
 
 // 绑定事件
 document.getElementById('get_data').addEventListener('click', function () {
+	// 统计页面数量
 	var page_data = count_pages().then((res) => {
-		// 统计页面数量
 		var resLength = res.length;
 		document.getElementById('page_number').innerHTML = resLength;
 	});
+	// 统计块数量和总字数
 	var block_data = count_blocks().then((res) => {
-		// 统计块数量和总字数
 		var resLength = res.length;
 		var count_character = 0;
 		for (var i = 0; i < resLength; i++) {
@@ -21,8 +21,8 @@ document.getElementById('get_data').addEventListener('click', function () {
 		document.getElementById('block_number').innerHTML = resLength;
 		document.getElementById('character_number').innerHTML = count_character;
 	});
+	// 统计当前块数量和字数
 	var present_block_data = count_present_blocks().then((res) => {
-		// 统计当前块数量和字数
 		// console.log(res);
 		var resLength = res.length;
 		var count_character = 0;
@@ -31,6 +31,16 @@ document.getElementById('get_data').addEventListener('click', function () {
 		}
 		document.getElementById('present_block_number').innerHTML = resLength;
 		document.getElementById('present_character_number').innerHTML = count_character;
+	});
+	// 统计今日字数
+	var today_character_data = count_today_character().then((res) => {
+		// console.log(res);
+		var resLength = res.length;
+		var count_character = 0;
+		for (var i = 0; i < resLength; i++) {
+			count_character += res[i].length;
+		}
+		document.getElementById('today_character_number').innerHTML = count_character;
 	});
 });
 
@@ -52,6 +62,28 @@ async function count_present_blocks() {
 	var page_id = self_id.root_id;
 	// console.log(page_id);
 	var sql_sentence = "select * from blocks where type = 'p' and (root_id = '" + page_id + "'" + " or path like '%" + page_id + "%')";
+	const res = await sql(sql_sentence);
+	return res;
+}
+
+// 统计今日字数
+async function count_today_character() {
+	var today = new Date();
+	var year = today.getFullYear();
+	// console.log(year);
+	var month = today.getMonth() + 1;
+	if (month < 10) {
+		month = '0' + month;
+	}
+	// console.log(month);
+	var day = today.getDate();
+	if (day < 10) {
+		day = '0' + day;
+	}
+	// console.log(day);
+	var today_string = year + '' + month + '' + day + '';
+	console.log(today_string);
+	var sql_sentence = "select * from blocks where type = 'p' and updated like '%" + today_string + "%'";
 	const res = await sql(sql_sentence);
 	return res;
 }
